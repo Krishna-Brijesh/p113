b049f392b0790a6077431bbfdeff8b40420e9cf4
Krishna-Brijesh <90083498+Krishna-Brijesh@users.noreply.github.com>
2021-12-08 18:16:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload(){
+   img = loadImage('https://i.pinimg.com/474x/02/48/7d/02487d886dc1864d5847be22345a4b0d.jpg');
+}
+
+function draw() {
+image(img,400,400,150,120);
+
+    fill(255,0,0);
+  stroke(255,0,0);
+  rect(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  rect(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  rect(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  rect(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(30, 20, 55, 55);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(30, 20, 55, 55);
+  }
+  
+  function setup() {
+createCanvas(800,800);
+  }
+
+ 
